70440bc51f6655e936f7aa77d1a2d2d6eced2993
Sudarev_D <89365265993@mail.ru>
2023-06-02 11:20:25 +0300

    add major func

--- str_func.py
+++ str_func.py
@@ -2,3 +2,9 @@ def title(str):
     new_str = str.upper()
     return new_str
 
+def major(str):
+    """
+    Делает все слова с заглавной буквы
+    """
+    new_str = str.title()
+    return new_str
