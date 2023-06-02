ea2a5afd3e74334946d10c8f11fb8e1b3e168fb1
Sudarev_D <89365265993@mail.ru>
2023-06-02 11:41:59 +0300

    add new func and docstring

--- str_func.py
+++ str_func.py
@@ -1,10 +1,13 @@
 def title(str):
+    """
+       Функция преобразует символы строки
+       к верхнему регистру
+    """
     new_str = str.upper()
     return new_str
 
-def major(str):
+def capitalize(str):
     """
-    Делает все слова с заглавной буквы
+    Делает заглавными первые буквы каждого слова в строке, поступившей на вход функции
     """
-    new_str = str.title()
-    return new_str
+    return str.capitalize
